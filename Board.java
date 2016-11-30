b7a2c4dd90b22c9916fcd3c5e8291fa7ff93d62c
Christian de la Cruz-Munoz <christian.delacruzmunoz+git@gmail.com>
2016-11-30 12:41:10 -0500

    Added one algorithm for testing. Fixed where the program hangs. Now has less verbose output.

--- Board.java
+++ Board.java
@@ -8,7 +8,7 @@ public class Board {
 		board = new boolean[10][10];
 	}
 
-	public void clear() {	
+	public void clear() {
 		board = new boolean[10][10];
 	}
 
@@ -18,76 +18,83 @@ public class Board {
 		clearableRows = new boolean[10];
 		clearableCols = new boolean[10];
 
-		/* Loop thru each row and validate.	*/
-		for (int i = 0, j = 0 ; i < board.length ; i++) {
-			clearableRows[i] = clearableCols[i]  = true;
-			
-			for (j = 0; j < board[i].length ; j++) {
+		/* Loop thru each row and validate. */
+		for (int i = 0, j = 0; i < board.length; i++) {
+			clearableRows[i] = clearableCols[i] = true;
+
+			for (j = 0; j < board[i].length; j++) {
 				clearableRows[i] = clearableRows[i] && board[i][j];
 				clearableCols[i] = clearableCols[i] && board[j][i];
 			}
-			if (clearableRows[i]) cleared++;
-			if (clearableCols[i]) cleared++;
+			if (clearableRows[i])
+				cleared++;
+			if (clearableCols[i])
+				cleared++;
 		}
 
 		/* CLEAR all clearable rows */
-		for (int i = 0, j = 0 ; i < clearableRows.length ; i++)
+		for (int i = 0, j = 0; i < clearableRows.length; i++)
 			if (clearableRows[i]) {
-				for (j = 0 ; j < board[i].length ; j++)
+				for (j = 0; j < board[i].length; j++)
 					board[i][j] = false;
-				System.out.println("Clearing Row #" + (i+1));
+				//System.out.println("Clearing Row #" + (i + 1));
 			}
 
 		/* CLEAR all clearable columns. */
-		for (int i = 0, j = 0 ; i < clearableCols.length ; i++)
+		for (int i = 0, j = 0; i < clearableCols.length; i++)
 			if (clearableCols[i]) {
-				for (j = 0 ; j < board.length ; j++)
+				for (j = 0; j < board.length; j++)
 					board[j][i] = false;
-				
-				System.out.println("Clearing Col #" + (i+1));
+
+				//System.out.println("Clearing Col #" + (i + 1));
 			}
 
 		return cleared;
-		/* TODO: ensure that intersecting fields do not affect score
-			in the wrong whey. */
+		/*
+		 * TODO: ensure that intersecting fields do not affect score in the
+		 * wrong whey.
+		 */
 	}
+
 	public ArrayList<Coordinate> getAvailableSpots(boolean[][] piece) {
 
 		ArrayList<Coordinate> results = new ArrayList<>();
 
-		for ( int i = 0 ; i < board.length - piece.length + 1; i++  ) {
-			for ( int j = 0 ; j < board[i].length - piece[0].length + 1; j++) {
-				/* so now we can work with each possible upper left corner...*/
+		for (int i = 0; i < board.length - piece.length + 1; i++) {
+			for (int j = 0; j < board[i].length - piece[0].length + 1; j++) {
+				/* so now we can work with each possible upper left corner... */
 
 				boolean works = true;
-				for (int k = 0 ; k < piece.length && works ; k++ ) {
-					for (int l = 0 ; l < piece[k].length && works; l++) {
-						/* check if there is a block at this cell in piece
-							AND the corresp. block in board is empty, OR the cell
-							in the piece is empty... */
-						works = (piece[k][l] && !board[i+k][j+l]) || !piece[k][l];
+				for (int k = 0; k < piece.length && works; k++) {
+					for (int l = 0; l < piece[k].length && works; l++) {
+						/*
+						 * check if there is a block at this cell in piece AND
+						 * the corresp. block in board is empty, OR the cell in
+						 * the piece is empty...
+						 */
+						works = (piece[k][l] && !board[i + k][j + l]) || !piece[k][l];
+						//System.out.println(k+" "+l+" "+i+" "+j+" "+works);
 					}
 				}
 
 				if (works)
-					results.add(new Coordinate( i , j ) );
-				
+					results.add(new Coordinate(i, j));
 			}
 		}
 		return results;
 	}
 
-	/* assumes that the piece is being inserted in a valid place.*/
+	/* assumes that the piece is being inserted in a valid place. */
 	public void placePiece(boolean[][] piece, Coordinate c) {
 		for (int i = 0; i < piece.length; i++) {
 			for (int j = 0; j < piece[0].length; j++) {
-				board[c.row+i][c.col+j] = board[c.row+i][c.col+j] || piece[i][j];	
+				board[c.row + i][c.col + j] = board[c.row + i][c.col + j] || piece[i][j];
 			}
 		}
 	}
 
 	public String toString() {
-		
+
 		String ret = "";
 		for (boolean[] arr : board) {
 			ret += "| ";
@@ -99,10 +106,43 @@ public class Board {
 	}
 
 	public void placePiece(Piece p, Coordinate c) {
-		placePiece(p.raw , c);
+		placePiece(p.raw, c);
 	}
 
 	public ArrayList<Coordinate> getAvailableSpots(Piece p) {
 		return getAvailableSpots(p.raw);
+	}
+	
+	public Coordinate getFirstAvailibleSpot(Piece p)
+	{
+		return getFirstAvailibleSpot(p.raw);
+	}
+	
+	public Coordinate getFirstAvailibleSpot(boolean[][] piece) {
+
+		Coordinate results;
+
+		for (int i = 0; i < board.length - piece.length + 1; i++) {
+			for (int j = 0; j < board[i].length - piece[0].length + 1; j++) {
+				/* so now we can work with each possible upper left corner... */
+
+				boolean works = true;
+				for (int k = 0; k < piece.length && works; k++) {
+					for (int l = 0; l < piece[k].length && works; l++) {
+						/*
+						 * check if there is a block at this cell in piece AND
+						 * the corresp. block in board is empty, OR the cell in
+						 * the piece is empty...
+						 */
+						works = (piece[k][l] && !board[i + k][j + l]) || !piece[k][l];
+						//System.out.println(k+" "+l+" "+i+" "+j+" "+works);
+					}
+				}
+
+				if (works)
+					return new Coordinate(i,j);
+			}
+		}
+		return null;
 	}
 }
